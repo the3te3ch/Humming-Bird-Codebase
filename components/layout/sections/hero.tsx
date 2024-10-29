3aec1c2f7d8339008bd44e1669e0bee2f7ed73c2
Lennox <72359822+theyardmic3@users.noreply.github.com>
2024-10-29 23:38:03 +0000

    Button Linking

--- components/layout/sections/hero.tsx
+++ components/layout/sections/hero.tsx
@@ -1,7 +1,5 @@
 "use client";
 import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
-import { ArrowRight } from "lucide-react";
 import Link from "next/link";
 
 interface RouteProps {
@@ -23,10 +21,10 @@ export const HeroSection = () => {
       <div className="grid place-items-center lg:max-w-screen-xl gap-8 mx-auto py-20 md:py-32">
         <div className="text-center space-y-8">
           <Badge variant="outline" className="text-sm py-2">
-            <span className="mr-2 text-sm ">
+            <span className="mr-2 text-sm">
               <Badge>Professional</Badge>
             </span>
-            <span className="text-sm"> Printing & Publishing Press</span>
+            <span className="text-sm">Printing & Publishing Press</span>
           </Badge>
 
           <div className="max-w-screen-md mx-auto text-center text-4xl md:text-6xl font-bold">
@@ -42,22 +40,12 @@ export const HeroSection = () => {
             {`The Ultimate Publication and Printing Press Company for All Your Book Design and Publishing needs.`}
           </p>
           
-          <div className="space-y-4 md:space-y-0 md:space-x-4">
-            <Link href={routeList[2].href} passHref>
-              <Button asChild variant="default" className="w-5/6 md:w-1/4 font-bold group/arrow">
-                <span className="flex items-center">
-                  Explore Catalog
-                  <ArrowRight className="size-5 ml-2 group-hover/arrow:translate-x-1 transition-transform" />
-                </span>
-              </Button>
+          <div className="pt-10 items-center justify-center space-y-3 sm:space-x-6 sm:space-y-0 sm:flex">
+            <Link href="/catalog" className="px-7 py-3 w-full bg-orange-700 text-white text-center rounded-md shadow-md block sm:w-auto">
+              Explore Our Catalog
             </Link>
-
-            <Link href={routeList[3].href} passHref>
-              <Button asChild variant="secondary" className="w-5/6 md:w-1/4 font-bold">
-                <span className="flex items-center">
-                  Learn More
-                </span>
-              </Button>
+            <Link href="/about" className="px-7 py-3 w-full border shadow-md rounded-lg border-orange-700 text-white text-center  block sm:w-auto">
+             Learn More
             </Link>
           </div>
         </div>
