699b74aa8ae454a75278b85b67235f3ba7ed194e
Lennox <72359822+theyardmic3@users.noreply.github.com>
2024-11-03 02:21:05 +0000

    styling: favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,16 +11,31 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Hummingbird Musikk Limited",
-  description: "Sing the Moment."
+  description: "Sing the Moment.",
+  icons: {
+    icon: '/Logo.png', // Link to your favicon
+  }
 }
 
-export default function RootLaeyout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="pt-br" suppressHydrationWarning>
+      <head>
+        {/* Favicon links */}
+        <head>
+
+          <link rel="icon" type="image/png" sizes="1302x1302" href="/Logo.png" />
+          <link rel="icon" type="image/png" sizes="316x316" href="/Logo.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/Logo.png" />
+          <link rel="icon" type="image/png" sizes="300x300" href="/Logo.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+        </head>
+
+      </head>
       <body className={cn("min-h-screen bg-background", inter.className)}>
         <ThemeProvider
           attribute="class"
@@ -29,7 +44,6 @@ export default function RootLaeyout({
           disableTransitionOnChange
         >
           <Navbar />
-
           {children}
           <SpeedInsights />
           <FooterSection />
