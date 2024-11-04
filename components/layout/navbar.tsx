1be90090b7ebf1d945e3e2bca37d46ab23167eee
Lennox <72359822+theyardmic3@users.noreply.github.com>
2024-11-04 23:26:07 +0000

    style: animation duration refactor to 300s for a smoother transition.

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -65,7 +65,7 @@ export const Navbar = () => {
                     href={href}
                     className="relative text-md font-semibold transition duration-200 ease-in-out
                                after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0
-                               after:bg-primary after:shadow-b-lg after:transition-all after:duration-600
+                               after:bg-primary after:shadow-b-lg after:transition-all after:duration-300
                                hover:after:w-full"
                   >
                     {label}
